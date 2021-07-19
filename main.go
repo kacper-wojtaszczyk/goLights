7992cfb437eb24c31084956ba9954bbdc830909d
Kacper Wojtaszczyk <kacper.wojtaszczyk95@gmail.com>
2021-07-19 18:37:50 +0300

    that's a fun effect

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/kacper-wojtaszczyk/goLights/mqtt"
 	"github.com/kacper-wojtaszczyk/goLights/music"
 	"github.com/zmb3/spotify"
+	"math/rand"
 	"os"
 	"time"
 )
@@ -16,7 +17,6 @@ func main() {
 	bulbNames := os.Args[2:]
 	godotenv.Load(".env.local", ".env")
 	client := mqtt.CreateMqttClient(os.Getenv("MQTT_BROKER_URI"), os.Getenv("MQTT_USERNAME"), os.Getenv("MQTT_PASSWORD"))
-	spotify := music.CreateSpotifyClient()
 	var lights []light.Light
 	for i := 0; i < len(bulbNames); i++ {
 		lights = append(lights, light.Create(bulbNames[i]))
@@ -27,7 +27,7 @@ func main() {
 		rainbowRotate(lightRepository, lights...)
 		break
 	case "spo":
-		spo(lightRepository, spotify, lights...)
+		spo(lightRepository, music.CreateSpotifyClient(), lights...)
 		break
 	case "turnOff":
 		turnOff(lightRepository, lights...)
@@ -36,7 +36,7 @@ func main() {
 		warmWhite(lightRepository, lights...)
 		break
 	default:
-		spo(lightRepository, spotify, lights...)
+		spo(lightRepository, music.CreateSpotifyClient(), lights...)
 	}
 }
 
@@ -79,6 +79,14 @@ func rainbowRotate(repository light.Repository, lights ...light.Light) {
 
 func spo(repository light.Repository, spotifyClient music.SpotifyClient, lights ...light.Light) {
 	trackAttributes := spotifyClient.GetCurrentTrackAttributes()
+	fmt.Printf(
+		"Tempo: %f	Energy: %f	Danceability: %f	Key: %d	Acousticness: %f\n",
+		trackAttributes.Tempo,
+		trackAttributes.Energy,
+		trackAttributes.Danceability,
+		trackAttributes.Key,
+		trackAttributes.Acousticness,
+	)
 	go refreshTrack(trackAttributes, spotifyClient)
 	time.Sleep(time.Second*5)
 
@@ -88,21 +96,24 @@ func spo(repository light.Repository, spotifyClient music.SpotifyClient, lights 
 		lights[i].SetFadeSpeed(2)
 	}
 	shift := 0
-	step := 15
+	step := 10
 	x := 0
 	for {
 		for i := 0; i < len(lights); i++ {
-			lights[i].SetHue(shift + x*step)
-			x = (x+1)%6
+			if i == 0 || rand.Intn(i+1) == 0 {
+				lights[i].SetHue(shift + x*step)
+			}
+			x = (x+1)%3
 		}
 
 		repository.Publish(lights...)
 		shift = (shift + step) % 360
-		tempo := int64(trackAttributes.Tempo)
+
+		tempo := int64(trackAttributes.Tempo*1000)
 		if tempo < 1 {
 			tempo = 100
 		}
-		time.Sleep(time.Duration(time.Minute.Nanoseconds()/tempo))
+		time.Sleep(time.Duration(time.Minute.Microseconds()/tempo))
 	}
 }
 
